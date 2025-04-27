a728f6fbb6104a91958df830e81f4c477672611e
Diego Tavares Franco <dfranco@PSG.LOCAL>
2025-04-26 20:41:09 -0400

    commit inicial

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import "dotenv/config";
+import express from "express";
+
+async function init() {
+  const port = Number(process.env.PORT) || 5000;
+  const url = process.env.BASE_URL || "localhost";
+
+  try {
+    const app = express();
+
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+
+    app.listen(port, () => {
+      console.log(`Servidor escutando a porta http://${url}:${port}`);
+    });
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+init();
